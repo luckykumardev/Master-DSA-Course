a1da82ebca756231be8b6a32c22e13ed8d06ed3b
Aman Shekhar <aman.shekhar4@gmail.com>
2024-03-17 11:09:51 +0530

    CloneALinkedListWithNextAndRandomPointer.java

--- class_07/CloneALinkedListWithNextAndRandomPointer.java
+++ class_07/CloneALinkedListWithNextAndRandomPointer.java
@@ -1,5 +1,38 @@
 package class_07;
 
+/**
+*
+* https://leetcode.com/problems/copy-list-with-random-pointer/
+*/
+
+
 public class CloneALinkedListWithNextAndRandomPointer {
 
+}
+
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        
+        Map<Node, Node> map = new HashMap<>();
+        
+        Node current = head;
+        while (current != null) {
+            map.put(current, new Node(current.val));
+            current = current.next;
+        }
+        
+        current = head;
+        
+        while (current != null) {
+            map.get(current).next = map.get(current.next);
+            map.get(current).random = map.get(current.random);
+            current = current.next;
+        }
+    
+        return map.get(head);
+    }
 }
